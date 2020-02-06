849cb353765074d862c59fafaebcba6c14cd21d6
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2020-02-05 21:18:52 -0600

    Update SelectionSort.java

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,10 +1,36 @@
-/*
-Given an array of integers, sort the elements in the array in ascending order. The selection sort algorithm should be used to solve this problem.
-
-Examples
-
-{1} is sorted to {1}
-{1, 2, 3} is sorted to {1, 2, 3}
-{3, 2, 1} is sorted to {1, 2, 3}
-{4, 2, -3, 6, 1} is sorted to {-3, 1, 2, 4, 6}
+/* *
+ * Given an array of integers, sort the elements in the array in ascending order. The selection sort algorithm should be used to solve this problem.
+ * 
+ * Examples
+ *
+ * {1} is sorted to {1}
+ * {1, 2, 3} is sorted to {1, 2, 3}
+ * {3, 2, 1} is sorted to {1, 2, 3}
+ * {4, 2, -3, 6, 1} is sorted to {-3, 1, 2, 4, 6}
 */
+
+public class Solution {
+    public int[] solve(int[] array) {
+        if (array == null || array.length == 0) {
+            return array;
+        }
+        
+        for (int i = 0; i < arrray.length - 1; i++) {
+            int min = i;
+            
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[min]) {
+                    min = j;
+                }
+            }
+            swap(array, i, min);
+        }
+        return array;
+    }
+    
+    public void swap(int[] array, int a, int b) {
+        int temp = array[a];
+        int array[a] = array[b];
+        int array[b] = temp;
+    }
+}
