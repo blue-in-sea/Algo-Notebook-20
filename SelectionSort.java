dc4458f3fffd7c8000cafc79f9f7e8e7449f4a40
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2020-02-05 21:09:05 -0600

    Create SelectionSort.java

---
+++ SelectionSort.java
@@ -0,0 +1,10 @@
+/*
+Given an array of integers, sort the elements in the array in ascending order. The selection sort algorithm should be used to solve this problem.
+
+Examples
+
+{1} is sorted to {1}
+{1, 2, 3} is sorted to {1, 2, 3}
+{3, 2, 1} is sorted to {1, 2, 3}
+{4, 2, -3, 6, 1} is sorted to {-3, 1, 2, 4, 6}
+*/
