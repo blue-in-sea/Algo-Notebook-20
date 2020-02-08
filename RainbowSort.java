171b8767dd1ab7609deec5f6d0dc301ee8c1d9da
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2020-02-07 23:35:32 -0600

    Update RainbowSort.java

--- RainbowSort.java
+++ RainbowSort.java
@@ -1,4 +1,13 @@
 public class RainbowSort {
 
-
+    public int[] rainbowSort(int[] array) {
+        if (array == null || ) {
+            
+        }
+        
+        return array;
+    }
 }
+
+
+// Bucket Sort used in 大量重复的情况
