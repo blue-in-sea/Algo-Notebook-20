b147aa4bddc296bcd4267321216e85417d4ed746
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2020-02-07 22:45:49 -0600

    Update QuickSort.java

--- QuickSort.java
+++ QuickSort.java
@@ -4,11 +4,21 @@ public class QuickSort {
     
     }
     
-    public void quickSort() {
-    
+    public void quickSort(int[] array, int left, int right) {
+        // base case
+        if (left > right) {
+            return;
+        }
+        
+        int pivotIndex = partition(array, left, right);
+ 
+        // pivot is already at its position, when we do the recursive call
+        // the two partitions, pivot should not be inlcude in any them
+        quickSort(array, left, pivotIndex - 1);
+        quickSort(array, pivotIndex + 1, right);
     }
     
-    public int partition() {
+    public int partition(int[] array, int left, int right) {
     
     }
     
@@ -16,7 +26,7 @@ public class QuickSort {
     
     }
 
-    public void swap () {
+    public void swap() {
     
     }
 }
