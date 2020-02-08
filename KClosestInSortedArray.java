66c1e5481f05449baebb5b74a842e2f88d723151
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2020-02-07 22:28:06 -0600

    Update KClosestInSortedArray.java

--- KClosestInSortedArray.java
+++ KClosestInSortedArray.java
@@ -9,16 +9,23 @@ public class KClosestInSortedArray {
             return new int[0];
         }
         
-        int l = helper();
+        int l = helper(array, target);
         int r = l + 1;
         int[] res = new int[k];
         
         for (int i = 0; i < k; i++) {
-        
-        
+            // we can advance left pointer, if
+            //  1) right pointer is out-of-bound
+            //  2) right pointer is not out-of-bound:
+            //     2.1 left pointer is not out-of-bound (since we are not assume k < array.length, left might be out-of-bound)
+            //  && 2.2 left pointer is closer to target
+            if (r > array.length || l >= 0 && target - array[l] <= array[r] - target) {
+                res[i] = array[l++];
+            } else {
+                res[i] = array[r--];
+            }
         }
-        
-        
+      
         return res;
     }
     
