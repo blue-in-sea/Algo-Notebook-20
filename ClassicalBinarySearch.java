191bd2b9a86018201ccc9985f4b25dd6a1973beb
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2020-02-07 21:44:46 -0600

    Update ClassicalBinarySearch.java

--- ClassicalBinarySearch.java
+++ ClassicalBinarySearch.java
@@ -1,3 +1,10 @@
+/**
+ * Given a target integer T and an integer array A sorted in ascending order, 
+ * find the index i such that A[i] == T or return -1 if there is no such index. 
+ *
+ * Assumption: There can be duplicate elements in the array, 
+ * and you can return any of the indices i such that A[i] == T.
+ */
 public class ClassicalBinarySearch {
 
     public int binarySearch(int[] array, int target) {
@@ -13,13 +20,24 @@ public class ClassicalBinarySearch {
             
             if (array[mid] == target) {
                 return mid;
-            } else if (array[mid] > target) {
-                l = mid - 1;   
+            } else if (array[mid] < target) {  // target 在 mid 的右区间
+                l = mid + 1;   
             } else {
-                r = mid + 1;
+                r = mid - 1;
             }
         }
         
         return -1;
     }
+    
+    public int binarySearch(
 }
+        
+/**
+ * Unit Test
+ * 1. array is null
+ * 2. array is empty
+ * 3. array contains duplications
+ * 4. target not found
+ * . . .
+ */
