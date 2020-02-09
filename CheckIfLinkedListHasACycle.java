9f7a6a5b487c7620861f83918a6d0da5cdf39928
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2020-02-08 21:34:12 -0600

    Update CheckIfLinkedListHasACycle.java

--- CheckIfLinkedListHasACycle.java
+++ CheckIfLinkedListHasACycle.java
@@ -1,5 +1,34 @@
-public class CheckIfLinkedListHasACycle {
-
-
-
+class ListNode {
+    public int value;
+    public ListNode next;
+    public ListNode(int value) {
+        this.value = value;
+        next = null;
+    }
 }
+
+public class CheckIfLinkedListHasACycle {
+    
+    public boolean checkCircle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            
+            if (sloww == fast) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+}
+
+// follow-up: how to find the beginning node 
+// 前部分一样。在第一次相遇后，把其中一个 node 移到开头，让两个以同步的速度来走。
+// slow 和 fast 差的是环长的整数倍
