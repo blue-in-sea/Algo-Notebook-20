57b9b4b0584f886d0dba9f989397af6e3020b3c1
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2020-02-08 19:08:03 -0600

    Update RainbowSort.java

--- RainbowSort.java
+++ RainbowSort.java
@@ -1,11 +1,35 @@
 public class RainbowSort {
 
     public int[] rainbowSort(int[] array) {
-        if (array == null || ) {
-            
+        if (array == null || array.length <= 1) {
+            return array;
         }
         
+        // Three pos to define four intervals: 
+        // 1. the left side of (i) is -1, exclusive of i
+        // 2. the right side of (k) is 1, exclusive of k
+        // 3. bewtween (i) and (j) is 0, exclusive of j
+        // 4. between (j) and (k) will be the area to be determined
+        int i = 0;
+        int j = 0;
+        int k = array.length - 1;
+        
+        while (j <= k) {
+            if (array[i] == -1) {
+                i++;
+            } else if (array[j] == 0) {
+                j++;
+            } else {
+                swap(array, j++, k--);
+            }
+        }
         return array;
+    }
+    
+    public void swap(int[] array, int a, int b) {
+        int temp = array[a];
+        array[a] = array[b];
+        array[b] = temp;
     }
 }
 
