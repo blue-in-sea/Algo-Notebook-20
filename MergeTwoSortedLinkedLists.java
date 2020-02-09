97baf073325ebc42382d42e092b08ef95fb31838
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2020-02-08 21:40:38 -0600

    Create MergeTwoSortedLinkedLists.java

---
+++ MergeTwoSortedLinkedLists.java
@@ -0,0 +1,17 @@
+class ListNode {
+    public int value;
+    public ListNode next;
+    public ListNode(int value) {
+        this.value = value;
+        next = null;
+    }
+}
+
+// When to use dummyHead
+// 1) we are not sure if the orginal head will change or not after the problem
+// 2) we need to build a new list from scratch
+// return dummyHead.next
+
+public class MergeTwoSortedLinkedLists {
+
+}
