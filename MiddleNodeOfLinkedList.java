78df02be74a165316d797db48766406080d6ef63
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2020-02-08 21:20:31 -0600

    Update MiddleNodeOfLinkedList.java

--- MiddleNodeOfLinkedList.java
+++ MiddleNodeOfLinkedList.java
@@ -8,6 +8,21 @@ class ListNode {
 }
 
 public class MiddleNodeOfLinkedList {
-
-
+    // Time: O(1.5n)
+    // Extra Space: O(1)
+    public ListNode findMiddle(ListNode head) {
+        // corner case
+        if (head == null || head.next == null) {
+            return head;
+        }
+        
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        return slow;
+    }
 }
