ab9a3a4bed12167a484335c75f02048654875b70
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2020-02-08 21:15:18 -0600

    Create MiddleNodeOfLinkedList.java

---
+++ MiddleNodeOfLinkedList.java
@@ -0,0 +1,13 @@
+class ListNode {
+    public int value;
+    public ListNode next;
+    public ListNode(int value) {
+        this.value = value;
+        next = null;
+    }
+}
+
+public class MiddleNodeOfLinkedList {
+
+
+}
