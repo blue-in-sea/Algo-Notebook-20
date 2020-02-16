a366ea4571fa391b73c908915c45cf9e7f426629
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2020-02-15 19:48:44 -0600

    Update MyQueueLL.java

--- MyQueueLL.java
+++ MyQueueLL.java
@@ -9,18 +9,29 @@ class ListNode {
 public class MyQueueLL {
     private ListNode head;
     private ListNode tail;
+    // single linked-list, insert (offer) from tail, delete (poll) from head
     
     public MyQueueLL() {
         head = tail = null;
     }
     
     public boolean isEmpty() {
-        
+        return head == null && tail == null;
     }
     
     public Integer poll() {
+        if (head == null) {
+            return null;
+        }
         
+        ListNode node = head;
+        head = head.next;
         
+        if (head == null) {
+            tail == null;
+        }
+        head.next = null;
+        return node.value;  
     }
     
     public Integer peek() {
