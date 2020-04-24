a24ee3e043e0dd8a2cdca314b50ecfde2ab7886e
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2020-04-23 22:52:12 -0500

    Update CourseSchedule.java

--- CourseSchedule.java
+++ CourseSchedule.java
@@ -10,6 +10,64 @@
  */
 
 public class CourseSchedule {
-
+  /**
+   * @param numCourses: a total of n courses
+   * @param prerequisites: a list of prerequisite pairs
+   * @return: true if can finish all courses or false
+   */
+  public boolean canFinish(int numCourses, int[][] prerequisites) {
+    if (numCourses == 0 || prerequisites.length == 0) {
+      return true;
+    }
+    
+    Map<Integer, List<Integer>> graph = buildGraph(numCourses, prerequisites);  // course map
+    Map<Integer, Integer> indegreeMap = getIndegree(graph);                        // indegree map
+    
+    // 把所有入度为0的点，放到BFS专用的队列中
+    Queue<Integer> queue = new ArrayDeque<>();
+    // 初始化拓扑序列为空
+    List<Integer> res = new ArrayList<>();
+    
+    for (int i = 0; i < numCourses; i++) {
+      if (!indegreeMap.containsKey(i)) {
+        queue.offer(i);
+        res.add(i);
+      }
+    }
+    
+    // 每次从队列中(queue)拿出一个点放到拓扑序列(BFS list)里，并将该点指向的所有点的入度减1
+    while (!queue.isEmpty()) {
+      Integer course = queue.poll();  // 如果修掉这门课
+      for (Integer nei : couseMap.get(course)) {
+        indegreeMap.put(nei, indegreeMap.get(nei) - 1); // 有多少高阶课的 prerequisites 减1了
+        
+        // 减去1之后入度变为0的点，也放入队列
+      
+      
+    }
+  }
+  
+  private Map<Integer, List<Integer>> buildGraph(int numCourses, int[][] prerequisites) {
+    Map<Integer, List<Integer>> map = new HashMap<>();
+    
+    for (int i = 0; i < numCourses; i++) {
+      int cur = prerequisites[i][0];
+      int pre = prerequisites[i][1];
+      map.get(pre).add(cur);
+    }
+    return map;
+  }
+  
+  private Map<Integer, Integer> getIndegree(Map<Integer, List<Integer>> graph) {
+    Map<Integer, Integer> indegreeMap = new HashMap<>();
+    
+    for (int i = 0; i < numCourses; i++) {
+      for (Integer nei : graph.get(i)) {
+        indegreeMap.put(nei, indegreeMap.getOrDefault(nei, 0) + 1);
+      }
+    }
+    
+    return indegreeMap;
+  }
 
 }
